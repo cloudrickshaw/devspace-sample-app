8e34b7223e37e32b48ee83d3d5c2b21dc1a21b5d
thapakazi <milans.thapa78@gmail.com>
2022-08-30 18:21:10 +0545

    log - add basic logs
    ref: https://gist.github.com/hoitomt/c0663af8c9443f2a8294

--- api/main.go
+++ api/main.go
@@ -3,10 +3,10 @@ package main
 import (
     "fmt"
     "net/http"
+	"log"
 )
 
-func hello(w http.ResponseWriter, req *http.Request) {
-
+func ping(w http.ResponseWriter, req *http.Request) {
     fmt.Fprintf(w, "PONG\n")
 }
 
@@ -21,8 +21,20 @@ func headers(w http.ResponseWriter, req *http.Request) {
 
 func main() {
 
-    http.HandleFunc("/ping_api", hello)
+    http.HandleFunc("/ping_api", ping)
     http.HandleFunc("/headers", headers)
 
-    http.ListenAndServe(":8080", nil)
+	log.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
+
+	err := http.ListenAndServe( ":8080", logRequest(http.DefaultServeMux))
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func logRequest(handler http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("%s %s %s\n", r.RemoteAddr, r.Method, r.URL)
+		handler.ServeHTTP(w, r)
+	})
 }
