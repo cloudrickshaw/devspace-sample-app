96759245829196ae622e21e29e03ff194492c7b1
thapakazi <milans.thapa78@gmail.com>
2022-08-30 22:32:21 +0545

    cleanup - cluster, use namespace

--- cluster/main.sh
+++ cluster/main.sh
@@ -2,8 +2,10 @@
 
 set -e
 
+[[ $1 == "--destroy" ]] &&  ctlptl delete -f templates/local-cluster.yaml && exit 0
 [[ $1 == "--re-create" ]] &&  ctlptl delete -f templates/local-cluster.yaml
 
 # create cluster
 ctlptl apply -f templates/local-cluster.yaml
 kubectl get namespace dev || kubectl create namespace dev
+devspace use namespace dev
